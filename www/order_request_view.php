6a627590b3b3bef6fed05970af312ec1652b2ba7
Yuqin Ying <yingy@vcu.edu>
2021-05-08 15:04:04 -0400

    more

---
+++ www/order_request_view.php
@@ -0,0 +1,87 @@
+<html>
+<head>
+<title>Restuarant Database - Owner</title>
+<?php require_once('header.php'); ?>
+
+<!-- Font Awesome library -->
+<script src="https://kit.fontawesome.com/c6c713cdbc.js"></script>
+
+<!-- JS libraries for datatables buttons-->
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
+
+<script src="js/order_request_view.js"></script>
+
+<!-- CSS for datatables buttons -->
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.dataTables.min.css"/>
+</head>
+
+<?php require_once('connection.php'); ?>
+
+<body>
+
+<div class="container-fluid mt-3 mb-3">
+	<h4>Order Requests</h4>
+	
+        	
+	<div class="table-responsive">
+		<table id="table-employee" class="table table-bordered table-striped">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Request Date</th>
+					<th>Requested By</th>
+					<th>Item Name</th>
+					<th>Quantity</th>
+					<th>Reason</th>
+					<th>Status</th>
+					<th>Reviewed By</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+		</table>
+	</div>
+</div>
+
+<div id="employee-modal" class="modal fade">
+	<div class="modal-dialog">
+		<form method="post" id="employee-form">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title">Review Request</h4>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+
+						<label>Status</label>
+						<select class="form-control" id="status">
+            			    <?php
+            			        $sqlQuery = "SELECT DISTINCT `status` FROM order_request ORDER BY `status` ASC";
+            			        $stmt = $conn->prepare($sqlQuery);
+            			        $stmt->execute();
+            			        while ($row = $stmt->fetch()) {
+            			            echo '<option>' .$row['status'].  '</option>';
+            			        }
+                            ?>
+            			</select>
+           
+
+					</div>
+				</div>
+				<div class="modal-footer">
+					<input type="hidden" name="ID" id="ID"/>
+					<input type="hidden" name="action" id="action" value=""/>
+					<input type="submit" name="save" id="save" class="btn btn-info" value="Save" />
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
+
+</body>
+</html>
--- www/owner.php
+++ www/owner.php
@@ -12,7 +12,7 @@
     <ul>
     	<li><a href="owner_functions.php">View/Edit Employee Info</a></li>
 	<li><a href="owner_request_off_view.php">View/Edit All Off Work Requests</a></li>
-	<li><a href="owner_request_order_view.php">View/Edit Order Requests</a></li>
+	<li><a href="order_request_view.php">View/Edit Order Requests</a></li>
 	<li><a href="logout.php">Log out</a></li>
     </ul>
 </div>
